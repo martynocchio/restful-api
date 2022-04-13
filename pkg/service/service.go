2635867f1cf943dd9863b26c58dc12adbddeeb4b
martynocchio <martynocchio@mail.ru>
2022-04-14 00:27:41 +0300

    created middleware

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -8,6 +8,7 @@ import (
 type Authorization interface {
 	CreateUser(user restful_api.User) (int, error)
 	GenerateToken(username, password string) (string, error)
+	ParseToken(token string) (int, error)
 }
 
 type List interface {
